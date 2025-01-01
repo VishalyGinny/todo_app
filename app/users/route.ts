a0f4aba4f89903211340ed48d1f02c9499944b2a
VishalyGinny <vishalyginny@gmail.com>
2025-01-01 14:16:13 +0800

    copied stuff

--- app/users/route.ts
+++ app/users/route.ts
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
+import connect from "@/lib/db";
+import User from "@/lib/modals/user";
 
-export async function GET() {
-    return new NextResponse("This is my first api.");
-}
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Error in fetching users" + error.message, {
+      status: 500,
+    });
+  }
+};
--- lib/db.ts
+++ lib/db.ts
@@ -1,30 +1,30 @@
 import mongoose from "mongoose";
 import { buffer } from "stream/consumers";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const connect = async () => {
-    const connectionState = mongoose.connection.readyState;
-    if (connectionState === 1) {
-        console.log("Already connected");
-        return;
-    }
+  const connectionState = mongoose.connection.readyState;
+  if (connectionState === 1) {
+    console.log("Already connected");
+    return;
+  }
 
-    if (connectionState === 2 || connectionState === 3) {
-        console.log("Waiting for connection to be established");
-        return;
-    }
+  if (connectionState === 2) {
+    console.log("Waiting for connection to be established");
+    return;
+  }
 
-    try {
-        mongoose.connect(MONGODB_URL!, {
-            dbName: 'next14api'
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.log(error);
-        throw new Error("Failed to connect to MongoDB");
-    }
-        }
+  try {
+    mongoose.connect(MONGODB_URI!, {
+      dbName: "next14restapi",
+      bufferCommands: true,
+    });
+    console.log("Connected");
+  } catch (error: any) {
+    console.log("Error:", error);
+    throw new Error("Error: ", error);
+  }
+};
 
 export default connect;
-
