108573689c78e7744892841a8571e6a380eb1963
VishalyGinny <vishalyginny@gmail.com>
2025-01-06 10:31:30 +0800

    added task

--- components/task/taskInput.tsx
+++ components/task/taskInput.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Task from "./task";
 
 interface TaskInputProps {
   tasks: {
