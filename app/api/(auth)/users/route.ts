df3ac650b637d62afd89099a1b1776dfcd34b7ec
VishalyGinny <vishalyginny@gmail.com>
2025-01-07 10:24:40 +0800

    trying-postman

---
+++ app/api/(auth)/users/route.ts
@@ -0,0 +1,35 @@
+import connect from "@/lib/db";
+import User from "@/lib/modals/user";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error: any) {
+    return new NextResponse("Error in fetching users" + error.message, {
+      status: 500,
+    });
+  }
+};
+
+//some of this code is just regenerated, not copied from the video.
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: "User created successfully" }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new NextResponse("Error in creating user" + error.message, {
+      status: 500,
+    });
+  }
+};
