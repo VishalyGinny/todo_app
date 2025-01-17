151da18f6def563fa53764d05039dabf5a650c16
VishalyGinny <168171930+VishalyGinny@users.noreply.github.com>
2025-01-17 02:46:16 +0000

    auth0? added to route.js

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,14 +1,14 @@
 import { connectMongoDB } from '$lib/mongo';
 import Task from '$lib/models/Task';
 import { NextResponse } from 'next/server';
-
+import { getAccessToken, withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 export async function POST(request) {
     const { title } = await request.json();
     // Log the result of connectMongoDB call
     console.log("MongoDB Connection Result:", await connectMongoDB());
     // Connect to MongoDB (database)
-    await connectMongoDB('mongodb://localhost:27017/test');
+    await connectMongoDB();
     await Task.create({ title });
     return NextResponse.json({ message: 'Task created successfully' }, { status: 201 });
 }
